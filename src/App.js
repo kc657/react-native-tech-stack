5150c6f433ff168b657169187e22b8a4ae875be7
kc657 <kvn.0218@gmail.com>
2017-10-04 22:16:55 -0700

    Added common files and Header into the Provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {Text, View} from 'react-native'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducers from './reducers'
+import {Header} from './components/common'
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  return (
+    <Provider store={createStore(reducers )}>
       <View>
-        <Text>
-          Welcome to React Native!
-        </Text>
+        <Header headerText="Tech Stack"/>
       </View>
-    );
-  }
+    </Provider>
+  )
 }
 
 export default App
